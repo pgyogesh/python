9b2f5adb79b4f50b766a58565b3ba644dbdbac09
Yogesh Jadhav <yogeshjadhav96@gmail.com>
2018-01-23 18:20:02 +0530

    Update log_analyzer.py

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import re
 import optparse
 import datetime
@@ -14,7 +14,7 @@ hour = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','1
 def get_errors():
     counter = 0
     for h in hour:
-        error_regex= '^\d\d\d\d-\d\d-\d\d %s:\d\d:\d\d.\d\d\d Error:' %h
+        error_regex= '^^\d\d\d\d-\d\d-\d\d %s:\d\d:\d\d.\d\d\d Error:' %h
         file = open(vLogFile,"r")
         for line in file:
             for match in re.finditer(error_regex,line,re.S):
@@ -82,33 +82,28 @@ def get_custom():
         file.close()
 
 def trim_log():
-    start_time = input("Please enter start time:\n[Format: HH:MM]=")
-    end_time = input("Please enter end time:\n[Format: HH:MM]=")
-    trim_time = datetime.datetime.now().strftime('%d%H%M%S')
-    output_file = 'trimmed_log_%s.txt' %trim_time
-    with open(vLogFile) as file:
-        for vline in file:
-            vDate = vline[0:10]
-            break
-        start_line = vDate + ' ' + start_time
-        end_line = vDate + ' ' + end_time
-        print("Start time:", start_line)
-        print("End time:", end_line)
-        for num, line in enumerate(file, 1):
-            if start_line in line:
-                start_line_number = num
-                break
-        for num, line in enumerate(file, 1):
-            if end_line in line:
-                end_line_number = num
-                break
-        file.close()
-    with open(vLogFile,"r") as file:
-        oFile = open(output_file,'a')
-        for num, line in enumerate(file, 1):
-            if num >= start_line_number and num <= end_line_number:
-                oFile.write(line)
-    print("%s Created" %output_file)
+        start_time = raw_input("Please enter start time:\n[Format: HH:MM]=")
+        end_time = raw_input("Please enter end time:\n[Format: HH:MM]=")
+        trim_time = datetime.datetime.now().strftime('%d%H%M%S')
+        output_file = 'trimmed_log_%s.txt' %trim_time
+        oFile = open(output_file, 'a')
+        with open(vLogFile,'r') as input_file:
+                do_write = False
+                for i, line in enumerate(input_file, 1):
+                        if i == 1:  # First line, so figure out the start/end markers
+                                vDate = line[0:10]
+                                start_line = vDate + ' ' + start_time
+                                end_line = vDate + ' ' +end_time
+                        if not do_write and line.startswith(start_line):  # If we need to start copying...
+                                do_write = True
+                                print('Starting to write from line %d' %i)
+                        if do_write:
+                                oFile.write(line)
+                        if line.startswith(end_line):  # Stop writing, we have everything
+                                print('Stopping write on line %d' %i)
+                                break
+        print("%s Created" %output_file)
+
 if __name__ == '__main__':
     print("1. Error\n2. Warning\n3. Notice\n4. Critical\n5. Custom")
     vChoice = int(input("Please choose your option from above options (Ex. Choose 2 for Warnings)\n="))
@@ -124,7 +119,7 @@ if __name__ == '__main__':
         get_custom()
     else:
         print("Wrong input.. Exiting...!")
-    trim_choice = input("Do you want to trim error log:Yy|Nn[Default=N]\n=")
+    trim_choice = raw_input("Do you want to trim error log:Yy|Nn[Default=N]\n=")
     if trim_choice == 'Y' or trim_choice == 'y':
         trim_log()
     else:
