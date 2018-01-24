ac9b80a47a140e54d7bf8449f39c6cdb4d12f183
Yogesh Jadhav <yogeshjadhav96@gmail.com>
2018-01-24 09:14:48 +0530

    Update log_analyzer.py

--- log_analyzer.py
+++ log_analyzer.py
@@ -3,73 +3,31 @@ import re
 import optparse
 import datetime
 
-parser = optparse.OptionParser()
+parser = optparse.OptionParser() # To enable command line options
 parser.add_option("-f","--file",dest="log_file",
                           action="store",help="Specify log file to be parsed")
 options, args = parser.parse_args()
 vLogFile=options.log_file
 
-hour = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23']
+hour = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23'] # To loop and check the log for every hour
 
-def get_errors():
+
+def get_graph(log_type):
     counter = 0
     for h in hour:
-        error_regex= '^^\d\d\d\d-\d\d-\d\d %s:\d\d:\d\d.\d\d\d Error:' %h
+        error_regex= '^^\d\d\d\d-\d\d-\d\d %s:\d\d:\d\d.\d\d\d %s:' %(h,log_type) #regex for error loge event
         file = open(vLogFile,"r")
-        for line in file:
+        for line in file: #For loop to check the match in every line
             for match in re.finditer(error_regex,line,re.S):
-                counter = counter + 1
+                counter = counter + 1 # Increases the counter if match found
         if counter != 0:
-            print("%s:%s:%s" %(h,counter * '*',counter))
-            #print(h,':',counter * '*',counter)
-        counter = 0
-        file.close()
-
-def get_warnings():
-    counter = 0
-    for h in hour:
-        error_regex= '^\d\d\d\d-\d\d-\d\d %s:\d\d:\d\d.\d\d\d Warning:' %h
-        file = open(vLogFile,"r")
-        for line in file:
-            for match in re.finditer(error_regex,line,re.S):
-                counter = counter + 1
-        if counter != 0:
-            print("%s:%s:%s" %(h,counter * '*',counter))
-            #print(h,':',counter * '*',counter)
-        counter = 0
-        file.close()
-
-def get_notices():
-    counter = 0
-    for h in hour:
-        error_regex= '^\d\d\d\d-\d\d-\d\d %s:\d\d:\d\d.\d\d\d Notice:' %h
-        file = open(vLogFile,"r")
-        for line in file:
-            for match in re.finditer(error_regex,line,re.S):
-                counter = counter + 1
-        if counter != 0:
-            print("%s:%s:%s" %(h,counter * '*',counter))
-            #print(h,':',counter * '*',counter)
-        counter = 0
-        file.close()
-
-def get_criticals():
-    counter = 0
-    for h in hour:
-        error_regex= '^\d\d\d\d-\d\d-\d\d %s:\d\d:\d\d.\d\d\d Critical:' %h
-        file = open(vLogFile,"r")
-        for line in file:
-            for match in re.finditer(error_regex,line,re.S):
-                counter = counter + 1
-        if counter != 0:
-            print("%s:%s:%s" %(h,counter * '*',counter))
-            #print(h,':',counter * '*',counter)
-        counter = 0
+            print("%s:%s:%s" %(h,counter * '*',counter)) # Prints the counter number of astericks if it is not equals to zero
+        counter = 0 # Resets the counter
         file.close()
 
 def get_custom():
     counter = 0
-    custom_regex = input("Please enter string/regex to search in log\n=")
+    custom_regex = raw_input("Please enter string/regex to search in log\n=")
     for h in hour:
         file = open(vLogFile,"r")
         for line in file:
@@ -77,18 +35,20 @@ def get_custom():
                 counter = counter + 1
         if counter != 0:
             print("%s:%s:%s" %(h,counter * '*',counter))
-            #print(h,':',counter * '*',counter)
         counter = 0
         file.close()
 
 def trim_log():
-        start_time = raw_input("Please enter start time:\n[Format: HH:MM]=")
-        end_time = raw_input("Please enter end time:\n[Format: HH:MM]=")
-        trim_time = datetime.datetime.now().strftime('%d%H%M%S')
-        output_file = 'trimmed_log_%s.txt' %trim_time
+        start_time = raw_input("Please enter start time:\n[Format: HH:MM]=") # Takes start time as input
+        end_time = raw_input("Please enter end time:\n[Format: HH:MM]=") # Takes end time as input
+        trim_time = datetime.datetime.now().strftime('%d%H%M%S') # For the trimmied file name Ex. DDHHMMSS(DateHourMinuteSecond)
+        output_file = 'trimmed_log_%s.txt' %trim_time # File name for trimmed log
         oFile = open(output_file, 'a')
         with open(vLogFile,'r') as input_file:
                 do_write = False
+                # Below block of code uses for loop to check every line with line numbers
+                # Checks line number on to get date from line number 1
+                # Sets True flag to write if start time found and sets False flag to write if end time found
                 for i, line in enumerate(input_file, 1):
                         if i == 1:  # First line, so figure out the start/end markers
                                 vDate = line[0:10]
@@ -108,13 +68,13 @@ if __name__ == '__main__':
     print("1. Error\n2. Warning\n3. Notice\n4. Critical\n5. Custom")
     vChoice = int(input("Please choose your option from above options (Ex. Choose 2 for Warnings)\n="))
     if vChoice == 1:
-        get_errors()
+        get_graph('Error')
     elif vChoice == 2:
-        get_warnings()
+        get_graph('Warning')
     elif vChoice == 3:
-        get_notices()
+        get_graph('Notice')
     elif vChoice == 4:
-        get_criticals()
+        get_graph('Critical')
     elif vChoice == 5:
         get_custom()
     else:
